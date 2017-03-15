cc451707aecba689d8bed3e5df490cf2e3736b16
Ashish kumar <ashish05182001@gmail.com>
2017-03-15 17:24:27 +0530

    Splash Screen

---
+++ app/src/main/java/sddtu/org/tasknetwork/SplashScreen.java
@@ -0,0 +1,36 @@
+package sddtu.org.tasknetwork;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.lang.reflect.AccessibleObject;
+
+/**
+ * Created by Dell on 3/15/2017.
+ */
+
+public class SplashScreen extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread thread=new Thread(){
+            @Override
+            public void run(){
+                try {
+                    sleep(3000);
+                    Intent intent=new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        };
+        thread.start();
+
+    }
+}
